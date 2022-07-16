58f4518693e60f12c4a449536074b2a7880d0813
Luis-Antonimo <luisantonimo@gmail.com>
2022-07-16 22:32:49 +0200

    ok
    ok

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace T05Ejercicio11
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = new int[10];
+            int suma = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Añada el " + (i + 1) + "º valor: ");
+                int valor = Convert.ToInt32(Console.ReadLine());
+                array[i] = valor;
+                suma += valor;
+            }
+            //Para la suma también se puede utilizar el método Sum
+            //de System.Linq
+            Console.WriteLine("La suma de todos los valores es: " + suma);
+        }
+    }
+}
